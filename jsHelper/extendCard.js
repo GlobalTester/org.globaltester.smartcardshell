5860ee601b471c8ca9227ce24f2936fe15e54cf4
Alexander May <alexander.may@hjp-consulting.com>
2011-11-11 13:11:00 +0000

    add syntax highlighting of APDUs and describing hover text (hover text is dummy implementation only, til now)
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.smartcardshell/trunk@337 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- jsHelper/extendCard.js
+++ jsHelper/extendCard.js
@@ -11,10 +11,10 @@ Card.prototype.gt_sendCommand = function(command) {
 
 Card.prototype.gt_sendPlain = function (command){
 	encodedResp = new ByteString("", HEX);
-	logCmdApdu = "=> Command APDU\n"+HexString.dump(command);
+	logCmdApdu = "=> Command APDU [\n"+HexString.dump(command)+"\n] C-APDU";
 	print(logCmdApdu);
 	resp = this.plainApdu(command);
-	logRespApdu = "<= Response APDU\n"+HexString.dump(resp);
+	logRespApdu = "<= Response APDU [\n"+HexString.dump(resp)+"\n] R-APDU";
 	print(logRespApdu);
 	print ("SW: "+this.SW.toString(HEX) + "   (" + this.SWMSG + ")");
 	return resp;
