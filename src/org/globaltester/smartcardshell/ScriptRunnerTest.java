ee0da29560535327edd7f3b170df7163cc2e60bf
Alexander May <alexander.may@hjp-consulting.com>
2012-02-22 14:47:54 +0000

    add ScriptRunnerTest which tests implementation of ScriptRunner
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.smartcardshell.test/trunk@675 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/smartcardshell/ScriptRunnerTest.java
@@ -0,0 +1,55 @@
+package org.globaltester.smartcardshell;
+
+import static org.junit.Assert.assertEquals;
+
+import opencard.core.service.SmartCard;
+
+import org.globaltester.smartcardshell.preferences.PreferenceInitializer;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mozilla.javascript.Context;
+
+/**
+ * Test on the implementaiton of the SkriptRunner
+ * 
+ * @author amay
+ * 
+ */
+public class ScriptRunnerTest {
+	
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		// make sure OCF is initialized with test values
+		if (SmartCard.isStarted()) {
+			SmartCard.shutdown();
+		}
+
+		// initialize OCF
+		System.setProperty("OpenCard.loaderClassName",
+				org.globaltester.smartcardshell.test.TestPropertyLoader.class
+						.getName());
+		SmartCard.start();
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		new PreferenceInitializer().initializeDefaultPreferences();
+		PreferencesPropertyLoader.restartAndInitializeOCF();
+	}
+
+	@Test
+	public void testInitialCardConfig() throws Exception {
+
+		// init JS ScriptRunner and Context
+		Context cx = Context.enter();
+		ScriptRunner sr = new ScriptRunner(cx, "");
+
+		String result = sr.executeCommand(cx,
+				"card.gt_getCardConfig(\"ICAO9303\",\"MRZ\")");
+
+		// asserts
+		assertEquals("Returned default MRZ does not match", "P<D<<MUSTERMANN<<ERIKA<<<<<<<<<<<<<<<<<<<<<<C11T002JM4D<<9608122F1310317<<<<<<<<<<<<<<<6", result);
+	}
+
+}
