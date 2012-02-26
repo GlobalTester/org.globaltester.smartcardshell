69e97c3396eb67f26e34e7ec8db2ef1802c67716
Lothar Bornemann <lothar.bornemann@hjp-consulting.com>
2012-02-26 17:55:52 +0000

    add valid chars for mrz check
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.smartcardshell/trunk@708 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- jsHelper/defineConstants.js
+++ jsHelper/defineConstants.js
@@ -14,6 +14,11 @@ SW_WarningProcessing = new Array("62", "63");
 SW_ExecutionError = new Array("64", "65", "66");
 SW_CheckingError = new Array("67", "68", "69", "6A", "6B", "6C", "6D", "6E", "6F");
 
+// define valid Chars for MRZ coding
+validAlphabeticChars = new Array ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+validSpecialChars = new Array ("<");
+validNumericChars = new Array ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+
 //Define names of failure, warning and response without data
 NO_DATA = new String("NODATA");
 FAILURE_TEXT = new String("Failure");
