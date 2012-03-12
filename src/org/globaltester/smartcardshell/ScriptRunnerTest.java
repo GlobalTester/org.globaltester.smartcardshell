5c86fe5eb7c3bf8d4d567dedfc610a29f9c01f1b
Alexander May <alexander.may@hjp-consulting.com>
2012-03-12 08:56:32 +0000

    Add TestCampaignExecution to manage CardConfigs
    This commit adds a TestCampaignExecution class which handles/represents a singele Execution of a complete TestCampaign (containing the according TestElementExecutions) 
    This implies that execution of single TestCases within one TestCampaign are no longer supported, as in this situation no direct mapping to the used CardConfig is possible.
    
    Persistence of CardConfigurations and and TestCampaignExcution still needs to be implemented.
    
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.smartcardshell.test/trunk@791 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/smartcardshell/ScriptRunnerTest.java
+++ src/org/globaltester/smartcardshell/ScriptRunnerTest.java
@@ -1,8 +1,6 @@
 package org.globaltester.smartcardshell;
 
 import static org.junit.Assert.assertEquals;
-
-import static org.globaltester.cardconfiguration.CardConfigManager.DEFAULT_CARD_CONFIG;
 import opencard.core.service.SmartCard;
 
 import org.globaltester.cardconfiguration.CardConfigManager;
@@ -47,7 +45,7 @@ public class ScriptRunnerTest {
 		Context cx = Context.enter();
 		ScriptRunner sr = new ScriptRunner(cx, "");
 		sr.init(cx);
-		sr.initCard(cx, "card", CardConfigManager.get(DEFAULT_CARD_CONFIG));
+		sr.initCard(cx, "card", CardConfigManager.getDefaultConfig());
 
 		String result = sr.executeCommand(cx,
 				"card.gt_getCardConfig(\"ICAO9303\",\"MRZ\")");
