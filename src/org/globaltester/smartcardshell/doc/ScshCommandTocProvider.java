7d9777bfb28ffa28ff3bd8478407d8e36b47dfef
Alexander May <alexander.may@hjp-consulting.com>
2012-05-29 09:39:51 +0000

    initial plug-in with no content
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.smartcardshell.doc/trunk@1089 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/smartcardshell/doc/ScshCommandTocProvider.java
@@ -0,0 +1,18 @@
+package org.globaltester.smartcardshell.doc;
+
+import org.eclipse.help.AbstractTocProvider;
+import org.eclipse.help.ITocContribution;
+
+public class ScshCommandTocProvider extends AbstractTocProvider {
+
+	public ScshCommandTocProvider() {
+		//default constructor for reflective invocation
+	}
+
+	@Override
+	public ITocContribution[] getTocContributions(String locale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
