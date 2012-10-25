3a8d6d8ee195199d7e9863c8ad1a3e67f39b2537
Holger Funke <holger.funke@hjp-consulting.com>
2012-10-25 12:39:39 +0000

    typo
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.smartcardshell/trunk@1249 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- jsHelper/defineConstants.js
+++ jsHelper/defineConstants.js
@@ -8,14 +8,14 @@ var HexString = new Packages.de.cardcontact.tlv.HexString();
 var ByteUtil = new Packages.org.globaltester.util.ByteUtil();
 var TLVUtil = new Packages.org.globaltester.util.TLVUtil();
 
-//define aqivalent cases for return codes
+//define equivalent cases for return codes
 SW_NoError = new Array("9000");
 SW_NormalProcessing = new Array("90", "61");
 SW_WarningProcessing = new Array("62", "63");
 SW_ExecutionError = new Array("64", "65", "66");
 SW_CheckingError = new Array("67", "68", "69", "6A", "6B", "6C", "6D", "6E", "6F");
 
-// define valid Chars for MRZ coding
+// define valid chars for MRZ coding
 validAlphabeticChars = new Array ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
 validSpecialChars = new Array ("<");
 validNumericChars = new Array ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
