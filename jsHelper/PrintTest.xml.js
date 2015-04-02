14a2568f1a75b8b3d7387ea6c5be0986dc89fac4
Anke Kölzer <anke.koelzer@hjp-consulting.com>
2015-04-02 08:02:03 +0000

    Simple test file to check if debugger can execute this.
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.smartcardshell/trunk@1280 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ jsHelper/PrintTest.xml.js
@@ -0,0 +1,15 @@
+                  
+function f(x)
+{return x+1}
+
+var y=7;
+print("------- Moin - this is PrintTest---------");
+print(Math.cos(Math.PI));
+
+print(f(7));
+print(f(y));
+
+print("------- Kontrenzier Dich!---------");
+
+
+                   
