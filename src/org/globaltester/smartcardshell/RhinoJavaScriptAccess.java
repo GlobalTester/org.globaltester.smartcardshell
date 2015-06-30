4a4a8d2b0fb02fcceb3354035b9de9724e882291
Anke Kölzer <anke.koelzer@hjp-consulting.com>
2015-06-30 14:15:20 +0000

    - New order for variables and methods according to programming guidelines - Comments updated - New member portNum to be able to let several debuggers communicate in parallel
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.smartcardshell/trunk@1355 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/smartcardshell/RhinoJavaScriptAccess.java
+++ src/org/globaltester/smartcardshell/RhinoJavaScriptAccess.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.globaltester.smartcardshell;
 
 import org.eclipse.wst.jsdt.debug.rhino.debugger.RhinoDebugger;
@@ -9,54 +6,95 @@ import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ContextFactory;
 
 /**
+ * This class organizes the access to the Rhino JavaScript evaluator and the
+ * Rhino JavaScript debugger. Before it is possible to evaluate JavaScript code,
+ * a context must be activated. If debug mode is wished, a debugger must be
+ * activated. This functionality is provided by this class. <br>
+ * To use this class construct an instance of it and then call
+ * {@link #activateContext(boolean)}. If a context is no longer needed, finish
+ * its use by calling {@link #exitContext()}.
+ * 
  * @author koelzer
- *
  */
 public class RhinoJavaScriptAccess {
 
 	/**
-	 * This interface class is used for {@link #debuggerStartedObj} and serves 
-	 * as a place holder which can be filled with
-	 * functionality in later versions if needed to make some debugger information 
-	 * accessible.
+	 * Port number for socket communication between the debugger thread and the
+	 * debugger launch thread. This class variable is settable from
+	 * modules which do not have access to the RhinoJavaScriptAccess and
+	 * debugger objects. It is used for initializing the {@link #portNum} value
+	 * in new RhinoJavaScriptAccess instances.
 	 */
-	interface IDebuggerInfo {}
-	
+	static protected String standardPortNum = "9000";
+
 	/**
-	 * If a JavaScript debugger is active, this member stores a reference to it. 
+	 * This interface class is used for {@link #debuggerStartedObj} and serves
+	 * as a place holder which can be filled with functionality in later
+	 * versions if needed to make some debugger information accessible.
+	 */
+	interface IDebuggerInfo {
+	}
+
+	/**
+	 * As long as this object is null, no debugger has been started yet. This is
+	 * static because it must be gettable from modules which do not have access
+	 * to RhinoJavaSriptAccess objects.
+	 */
+	static protected IDebuggerInfo debuggerStartedObj = null;
+
+	/**
+	 * For JavaScript access a context must be activated. The context factory
+	 * delivers a new context respectively the context for the current thread. <br>
+	 * NOTE: This context factory is accessible from other modules using
+	 * ContextFactory.getGlobal(), see {@link #ContextFactory} for details.
+	 */
+	protected static ContextFactory contextFactory = createContextFactory();
+
+	/**
+	 * If a JavaScript debugger is active, this member stores a reference to it.
 	 * Otherwise it is set to null.
 	 */
 	protected RhinoDebugger debugger = null;
-	
+
 	/**
-	 * Port number for socket communication. This is static because it must be 
-	 * settable from modules which do not have access to the debugger object.
+	 * Indicates if this instance supports JavaScript debugging and a Rhino
+	 * debugger is or is going to be connected.
 	 */
-	static protected String standardPortNum = "9000";
-	//TODO debuggers should additionally get an individual port number 
-	
+	protected boolean debugMode = false;
+
 	/**
-	 * @return {@link #standardPortNum}
+	 * Individual port number of this instance for the socket communication
+	 * between the debugger thread and the debugger launch thread. It can be
+	 * used when one wants to work with several debuggers on different ports.<br>
+	 * NOTE: This functionality (working with several debuggers) is prepared
+	 * for later versions, but not yet implemented. 
 	 */
-	public static String getStandardPortNum() {
-		return standardPortNum;
+	protected String portNum = "9000";
+
+	/**
+	 * Constructor initializes {@link #portNum} with the value of
+	 * {@link #standardPortNum}.
+	 */
+	public RhinoJavaScriptAccess() {
+		portNum = standardPortNum;
 	}
 
 	/**
-	 * @see #standardPortNum
-	 * @param standardPortNum the standardPortNum to set
+	 * Collects the class loaders for all protocol extensions in smartcardshell
+	 * and creates a compound class loader from them. This is needed for the
+	 * initialization of the {@link #contextFactory}.
+	 * 
+	 * @return compound class loader
 	 */
-	public static void setStandardPortNum(String standardPortNum) {
-		RhinoJavaScriptAccess.standardPortNum = standardPortNum;
+	protected static ClassLoader getCompoundClassLoaderForProtocols() {
+		CompoundClassLoader compLoader = new CompoundClassLoader();
+		for (IScshProtocolProvider curProtocolProvider : ProtocolExtensions
+				.getInstance().getAllAvailableProtocols()) {
+			compLoader
+					.addClass(curProtocolProvider.getClass().getClassLoader());
+		}
+		return compLoader;
 	}
-
-
-	/**
-	 * If this object with anonymous interface type is null, no debugger has been
-	 * started yet. This is static because it must be 
-	 * gettable from modules which do not have access to the debugger object.
-	 */
-	static protected IDebuggerInfo debuggerStartedObj = null; //TODO how use this object correctly?
 
 	/**
 	 * @return {@link #debuggerStartedObj}
@@ -66,134 +104,52 @@ public class RhinoJavaScriptAccess {
 	}
 
 	/**
-	 * If a JavaScript context is active, this member stores a reference to its factory. 
-	 * Otherwise it is set to null.
-
+	 * @return {@link #standardPortNum}
 	 */
-	protected ContextFactory contextFactory = null;
-
-	/**
-	 * Pure constructor (does nothing special)
-	 */
-	public RhinoJavaScriptAccess() {
-		// TODO Auto-generated constructor stub
+	public static String getStandardPortNum() {
+		return standardPortNum;
 	}
 
 	/**
-	 * @return the contextFactory
+	 * @see #standardPortNum
+	 * @param standardPortNum
+	 *            the standardPortNum to set
 	 */
-	public ContextFactory getContextFactory() {
+	public static void setStandardPortNum(String standardPortNum) {
+		RhinoJavaScriptAccess.standardPortNum = standardPortNum;
+	}
+
+	/**
+	 * Creates the {@link #contextFactory} and initializes an appropriate
+	 * compound class loader for it, using loaders for protocol extensions,
+	 * compare {@link #getCompoundClassLoaderForProtocols()}.<br>
+	 * This method is automatically called on system start and must not be
+	 * called elsewhere.
+	 */
+	protected static ContextFactory createContextFactory() {
+
+		contextFactory = new ContextFactory();
+		ContextFactory.initGlobal(contextFactory); // makes this context factory
+													// accessible
+		// from other modules using ContextFactory.getGlobal()
+
+		// here we add an appropriate class loader to avoid the
+		// class load problem of org.mozilla.javascript plugin
+		contextFactory
+				.initApplicationClassLoader(getCompoundClassLoaderForProtocols());
 		return contextFactory;
 	}
 
 	/**
-	 * @param contextFactory
-	 *            the contextFactory to set
+	 * Makes some necessary settings for the Rhino debugger thread: socket
+	 * number, traces on/off, suspend yes/no. Then the debugger is started and
+	 * {@link #debuggerStartedObj} is created to inform other modules about
+	 * this.
+	 * 
+	 * @param curPortNum
+	 *            port number for socket to be used by the debugger thread
 	 */
-	public void setContextFactory(ContextFactory contextFact) {
-		contextFactory = contextFact;
-	}
-
-	/**
-	 * @return {@link #debugger}
-	 */
-	public RhinoDebugger getDebugger() {
-		return debugger;
-	}
-
-	/**
-	 * @param debugger
-	 *            the {@link #debugger} to set
-	 */
-	public void setDebugger(RhinoDebugger rhDebugger) {
-		debugger = rhDebugger;
-	}
-
-	/**
-	 * Collects the class loaders for all protocol extensions in smartcardshell
-	 * and creates a composite class loader from them.
-	 * @return composite class loader
-	 */
-	static protected ClassLoader getCompositeClassLoaderForProtocols() {
-		CompoundClassLoader compLoader = new CompoundClassLoader();
-		for (IScshProtocolProvider curProtocolProvider : ProtocolExtensions
-				.getInstance().getAllAvailableProtocols()) {
-			compLoader.add(curProtocolProvider.getClass().getClassLoader());
-		}
-		return compLoader;
-
-	}
-
-	/**
-	 * Generates a new context if necessary or activates an existing one and enters it. If debugMode==true, 
-	 * the Rhino JavaScript debugger will be started.
-	 * activateContext() must always be followed by {@link #closeContext()} 
-	 * when JavaScript access is finished!
-	 * @param debugMode indicates if the debugger should be started or not
-	 * @return the activated context
-	 */
-	public Context activateContext(boolean debugMode) {
-		Context cx = null;
-
-		if (contextFactory == null) {
-			contextFactory = createContextFactory();
-		}
-
-		if (debugMode) {
-			startDebugging();
-		}
-
-		// this always delivers the current context (if none is there, it will be generated)
-		cx = contextFactory.enterContext();
-		return cx;
-	}
-
-	/**
-	 * Stops the JavaScript debugger, if any is active, and exits the current context.
-	 */
-	public void closeContext() {
-		stopJSDebugger();
-		Context.exit();
-	}
-
-	/**
-	 * Creates a new context factory, if there was none. An appropriate class loader 
-	 * for protocol extensions is initialized for this factory in case it was newly 
-	 * created, compare {@link #getCompositeClassLoaderForProtocols()}.
-	 * @return the {@link #contextFactory}
-	 */
-	protected ContextFactory createContextFactory() {
-
-		if (contextFactory == null) {
-			contextFactory = new ContextFactory();
-			// here we add an appropriate class loader to avoid the
-			// class load problem of org.mozilla.javascript plugin
-			contextFactory
-					.initApplicationClassLoader(getCompositeClassLoaderForProtocols());
-		}
-
-		return contextFactory;
-	}
-
-	public void startDebugging() {
-		try {
-			startJSDebugger(standardPortNum);
-
-			if (debugger != null) {
-				contextFactory.addListener(debugger);
-			}
-		} catch (Exception exc) {
-			if (debugger != null)
-				contextFactory.removeListener(debugger);
-			stopJSDebugger();
-			System.err
-					.println("JavaScript Rhino debugger could not be started!");
-			System.err.println("Reason:\n" + exc.getMessage());
-			exc.printStackTrace();
-		}
-	}
-
-	protected void startJSDebugger(String portNum) {
+	protected void startJSDebugger(String curPortNum) {
 
 		System.out.println("Trying to start Rhino debugger ...");
 
@@ -202,13 +158,15 @@ public class RhinoJavaScriptAccess {
 		// trace=y: status should be reported to the Eclipse console
 		// simply delete this if you do not want traces
 		// String rhino = "transport=socket,suspend=y,trace=y,address=9000";
-		String rhino = "transport=socket,suspend=y,address=" + portNum;
+		String rhino = "transport=socket,suspend=y,address=" + curPortNum;
 
 		try {
 			// TODO write some log message somewhere??
 			debugger = new RhinoDebugger(rhino);
-			// create debuggerStartedObject which is used in debug command handler
-			debuggerStartedObj = new IDebuggerInfo() {};
+			// create debuggerStartedObject which is used in debug command
+			// handler
+			debuggerStartedObj = new IDebuggerInfo() {
+			};
 			debugger.start();
 			System.out.println("Debugger started!");
 		} catch (Exception e) {
@@ -220,15 +178,36 @@ public class RhinoJavaScriptAccess {
 		}
 	}
 
+	/**
+	 * Starts the debugger thread (see {@link #startJSDebugger(String)}) and
+	 * adds a listener for the debugger to the context factory.
+	 */
+	protected void startDebugging() {
+		try {
+			startJSDebugger(portNum);
+
+			if (debugger != null) {
+				contextFactory.addListener(debugger);
+			}
+		} catch (Exception exc) {
+			stopJSDebugger();
+			System.err // TODO send this info to the calling modules
+					.println("JavaScript Rhino debugger could not be started!");
+			System.err.println("Reason:\n" + exc.getMessage());
+			exc.printStackTrace();
+		}
+	}
 
 	/**
-	 * If there is an active JavaScript debugger, it is stopped.
+	 * If there is an active JavaScript debugger, it is stopped. The appropriate
+	 * values are reset.
 	 */
 	protected void stopJSDebugger() {
 		if (debugger == null)
 			return;
 
 		try {
+			contextFactory.removeListener(debugger);
 			debuggerStartedObj = null;
 			debugger.stop();
 			debugger = null;
@@ -239,4 +218,55 @@ public class RhinoJavaScriptAccess {
 		}
 	}
 
+	/**
+	 * @return {@link #portNum}
+	 */
+	public String getPortNum() {
+		return portNum;
+	}
+
+	/**
+	 * Sets {@link #portNum}.
+	 * 
+	 * @param portNum
+	 */
+	public void setPortNum(String portNum) {
+		this.portNum = portNum;
+	}
+
+	/**
+	 * Delivers a new context - respectively the context connected to the
+	 * current thread - and activates it. If debugMode==true, the Rhino
+	 * JavaScript debugger will be started. activateContext() must always be
+	 * followed by {@link #exitContext()} when JavaScript access is finished!
+	 * 
+	 * @param newDebugMode
+	 *            indicates if the debugger should be started or not
+	 * @return the activated context
+	 */
+	public Context activateContext(boolean newDebugMode) {
+		Context cx = null;
+
+		if (newDebugMode) {
+			debugMode = true;
+			startDebugging();
+		}
+
+		// this always delivers the current context (if none is there, it will
+		// be generated)
+		cx = contextFactory.enterContext();
+		return cx;
+	}
+
+	/**
+	 * Stops the JavaScript debugger, if in debug mode, and exits the current
+	 * context.
+	 */
+	public void exitContext() {
+		if (debugMode) {
+			stopJSDebugger();
+			debugMode = false;
+		}
+		Context.exit();
+	}
 }
