7c2acb85c8a3522dc716b367b56e46471b9f57da
Anke Kölzer <anke.koelzer@hjp-consulting.com>
2015-07-09 09:12:56 +0000

    Comment for review added, since the following line is commented out and could cause problems: //"jsFunction_parse": name "parse" is already in use".
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.smartcardshell/trunk@1380 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- scsh3.7.989/config.js
+++ scsh3.7.989/config.js
@@ -127,9 +127,10 @@ defineClass("de.cardcontact.scdp.gp.Card");
 defineClass("de.cardcontact.scdp.gp.GPKey");
 defineClass("de.cardcontact.scdp.gp.GPCrypto");
 
-//TODO AKR: the next line is commented out because it causes an org.mozilla.javascript.EvaluatorException: 
+//TODO amay, TODO AKR: the next line is commented out because it causes an org.mozilla.javascript.EvaluatorException: 
 //"jsFunction_parse": name "parse" is already in use".
 //check if this must be replaced somehow!
+//How can we test, if this causes problems???
 //defineClass("de.cardcontact.scdp.gp.GPXML");
 
 defineClass("de.cardcontact.scdp.gp.GPTLV");
